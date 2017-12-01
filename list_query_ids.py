991caf7cb7fa38da80a2f7829f56c6d183723081
Thomas Hartland <h4rtland@gmail.com>
2017-12-01 11:35:19 +0000

    Clean list_query_ids slightly

--- list_query_ids.py
+++ list_query_ids.py
@@ -4,14 +4,13 @@ import boto3
 athena = boto3.client("athena")
 
 response = athena.list_named_queries()
-#print(response)
 
 table = PrettyTable(["Name", "Query ID"])
 table.align["Name"] = "l"
 
 for query_id in response["NamedQueryIds"]:
     query = athena.get_named_query(NamedQueryId=query_id)
-    #print(query["NamedQuery"]["Name"], "\t\t", query_id)
+    print(query)
     table.add_row([query["NamedQuery"]["Name"], query_id])
 
 print(table)
