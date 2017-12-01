629338d2aa6aeaa179b0e8596ae71d176e023719
Thomas Hartland <h4rtland@gmail.com>
2017-12-01 11:37:34 +0000

    Remove testing print from list_query_ids

--- list_query_ids.py
+++ list_query_ids.py
@@ -10,7 +10,6 @@ table.align["Name"] = "l"
 
 for query_id in response["NamedQueryIds"]:
     query = athena.get_named_query(NamedQueryId=query_id)
-    print(query)
     table.add_row([query["NamedQuery"]["Name"], query_id])
 
 print(table)
