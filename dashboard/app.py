02893cc915b5c53eb42d7410a5cacafe7e47bb0b
H4rtland <h4rtland@gmail.com>
2017-12-11 23:03:21 +0000

    Add click handler to queue names in data table and style them as links

--- dashboard/app.py
+++ dashboard/app.py
@@ -4,6 +4,7 @@ import dash
 import flask
 
 SCRIPT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'scripts')
+CSS_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'css')
 
 app = dash.Dash()
 server = app.server
@@ -14,7 +15,12 @@ app.config.suppress_callback_exceptions = True
 def serve_script(resource):
     return flask.send_from_directory(SCRIPT_PATH, resource)
 
+@app.server.route('/css/<resource>')
+def serve_css(resource):
+    return flask.send_from_directory(CSS_PATH, resource)
+
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+app.css.append_css({"external_url": "/css/index.css"})
 
 app.scripts.append_script({"external_url": "https://code.jquery.com/jquery-3.2.1.min.js"})
 app.scripts.append_script({"external_url": "https://rawgit.com/pie6k/jquery.initialize/master/jquery.initialize.min.js"})
--- dashboard/scripts/fix_datatable.js
+++ dashboard/scripts/fix_datatable.js
@@ -5,3 +5,13 @@ $.initialize("div.react-grid-Toolbar .tools button.btn", function() {
 $.initialize("div.react-grid-Toolbar", function() {
 	$(this).hide();
 });
+
+$.initialize("div.react-grid-Cell .react-grid-Cell__value div span div", function() {
+    if ($(this)[0].parentElement.parentElement.parentElement.parentElement.style.left == "0px") {
+        $(this)[0].onclick = function () {
+            window.history.pushState({}, '', "/queue/" + $(this)[0].title);
+            window.dispatchEvent(new Event('onpushstate'));
+            return false;
+        };
+    }
+});
