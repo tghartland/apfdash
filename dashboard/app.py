5f4a4597fbeaa07b5f02dbda2f49ac338d55bee1
H4rtland <h4rtland@gmail.com>
2017-12-11 17:15:31 +0000

    Load js script from github via rawgit.com

--- dashboard/app.py
+++ dashboard/app.py
@@ -8,4 +8,8 @@ app.config.suppress_callback_exceptions = True
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 app.scripts.append_script({"external_url": "https://code.jquery.com/jquery-3.2.1.min.js"})
-app.scripts.append_script({"external_url": "https://raw.githubusercontent.com/H4rtland/athena_project/master/dashboard/scripts/fix_datatable.js"})
+
+# Load js script via rawgit
+#  - github prevents direct loading of js scripts by setting the MIME type to text/plain
+# Trying to host the file locally is a pain because dash intercepts all routes and tries to return a dashboard for them (which fails)
+app.scripts.append_script({"external_url": "https://rawgit.com/H4rtland/athena_project/master/dashboard/scripts/fix_datatable.js"})
