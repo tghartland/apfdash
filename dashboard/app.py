62c98f974449fa62dfec1fb9f61072159001eb54
H4rtland <h4rtland@gmail.com>
2017-12-11 18:33:56 +0000

    Change to using jquery initialize plugin
    Previous solution didn't account for DataTable being reloaded WITHOUT the page being refreshed when switching between apps, so only on the first page load would the script run.
    
    New solution works in this case and doesn't show the toolbar for a split second before hiding it. All around better.
    
    Initialize plugin works only in IE9+ (please let this not be an issue).

--- dashboard/app.py
+++ dashboard/app.py
@@ -8,6 +8,7 @@ app.config.suppress_callback_exceptions = True
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 app.scripts.append_script({"external_url": "https://code.jquery.com/jquery-3.2.1.min.js"})
+app.scripts.append_script({"external_url": "https://rawgit.com/pie6k/jquery.initialize/master/jquery.initialize.min.js"})
 
 # Load js script via rawgit
 #  - github prevents direct loading of js scripts by setting the MIME type to text/plain
