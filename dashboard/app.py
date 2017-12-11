9887c7bcfaa4ade10876272d6667fe9bec48f8e1
H4rtland <h4rtland@gmail.com>
2017-12-11 17:07:39 +0000

    Load js script from github rather than pastebin

--- dashboard/app.py
+++ dashboard/app.py
@@ -8,4 +8,4 @@ app.config.suppress_callback_exceptions = True
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 app.scripts.append_script({"external_url": "https://code.jquery.com/jquery-3.2.1.min.js"})
-app.scripts.append_script({"external_url": "https://pastebin.com/raw/DiXVQWvc"})
+app.scripts.append_script({"external_url": "https://raw.githubusercontent.com/H4rtland/athena_project/master/dashboard/scripts/fix_datatable.js"})
